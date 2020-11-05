5c7d72d8ee4405560dd62b8b77308d31631e9cc1
Guo-Yutong <2000022773@stu.pku.edu.cn>
2020-11-05 10:28:13 +0800

    update

--- Course.java
+++ Course.java
@@ -28,8 +28,27 @@ public class Course implements Cloneable{
 		students[Arrays.binarySearch(students,student)] = null;
 	}
 	
-	@Override
+	@Override	//重写方法，实现students数据域的深度拷贝
 	public String[] clone() {
 		return students.clone();
+	}
+	
+	//测试用例
+	public static void main(String args[]) {
+		Course a = new Course("Java");
+		Course b = new Course("Python");
+		
+		a.addStudent("小明");
+		a.addStudent("小刚");
+		a.addStudent("小红");
+		
+		b.students = a.clone();
+		System.out.println("Java课原始学生: " + Arrays.toString(a.students));
+		System.out.println("Python课原始学生: " + Arrays.toString(b.students));
+		
+		a.addStudent("小亮");
+		b.addStudent("小军");
+		System.out.println("Java课增选后的学生: " + Arrays.toString(a.students));
+		System.out.println("Python课增选后的学生: " + Arrays.toString(b.students));
 	}
 }
